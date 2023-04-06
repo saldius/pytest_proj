811cc7d96865fd5107617e7f62d8016b8778cb00
serg shutin <saldius@gmail.com>
2023-04-07 02:06:13 +0900

    fix test 2 def test_get

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,7 +3,7 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([0], 0, "test") == 0
 
 
 def test_slice():
